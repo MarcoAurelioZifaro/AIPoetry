fb5f8b89933d4525e1eb289323ab0c3f0d8bc2ec
Marco Aurelio Zifaro <marcoaureliozifaro@Marcos-MacBook-Pro.local>
2019-02-24 15:16:40 +0000

    Version 2: Styled and animated Header

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,17 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="PoemList">
+        <div className ="PoemSingle">
+          <p>Name Devie Date and Time </p>
+          <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+        </div>
     </div>
     <Link to="/page-2/">Go to page 2</Link>
+
+
   </Layout>
 )
 
 export default IndexPage
+
